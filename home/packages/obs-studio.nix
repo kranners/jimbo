59411ac472abcd91a85d1aace78f0ace36966875
Aaron Pierce <aaron@cute.engineer>
2024-04-06 19:03:08 +1100

    feat(obs): adding replay saving bind

--- home/packages/obs-studio.nix
+++ home/packages/obs-studio.nix
@@ -4,6 +4,37 @@
   lib,
   inputs,
   ...
-}: {
+}: let
+  modifier = config.wayland.windowManager.sway.config.modifier;
+
+  save-replay = pkgs.writeShellApplication {
+    name = "save-replay";
+
+    runtimeInputs = [pkgs.obs-cmd];
+
+    text = ''
+      obs-cmd replay save && notify-send "Replay saved to ~/replays/Replay $(date '+%Y-%m-%d %H-%M-%S').mp4"
+    '';
+  };
+in {
   programs.obs-studio.enable = true;
+  home.packages = [save-replay];
+
+  systemd.user.services.obs = {
+    Unit = {
+      Description = "OBS Studio, mainly for screen recordings and replays";
+    };
+    Install = {
+      WantedBy = ["sway-session.target"];
+    };
+    Service = {
+      ExecStart = "${pkgs.obs-studio}/bin/obs --startreplaybuffer --minimize-to-tray";
+      Restart = "always";
+      RestartSec = "3";
+    };
+  };
+
+  wayland.windowManager.sway.config.keybindings = {
+    "${modifier}+Shift+r" = "exec save-replay";
+  };
 }
