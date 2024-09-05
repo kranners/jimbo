c6f2022a6b65ef8bc56708ff2f578fe660d4e653
Aaron Pierce <aaron@cute.engineer>
2024-09-05 15:43:21 +1000

    feat(obsidian): adding bind for creating new zettel note

--- shared/modules/nixvim/plugins/obsidian.nix
+++ shared/modules/nixvim/plugins/obsidian.nix
@@ -39,15 +39,49 @@
         action.__raw = ''
           function()
             local client = require("obsidian").get_client()
+            local util = require("obsidian.util")
 
-            if client.current_note(client) == nil then
-                return vim.cmd("ObsidianToday")
-            else
-                return vim.cmd("e#")
+            -- If we are currently in a floating window, just close that instead
+            local win = vim.api.nvim_get_current_win()
+            local win_config = vim.api.nvim_win_get_config(win)
+
+            local floating = win_config.relative == "editor"
+            if floating then
+              vim.api.nvim_buf_delete(vim.api.nvim_win_get_buf(win), { force = true })
+              return
             end
+
+            -- Otherwise, create a new note with a given title
+            local title = util.input("Enter title", { completion = "file" })
+            if title == "" or title == nil then
+              vim.notify("Provide a title for the note")
+              return
+            end
+
+            local note = client:create_note({ title = title, no_write = true, dir = "Stack" })
+
+            local width = math.ceil(math.min(150, vim.o.columns * 0.8))
+            local height = math.ceil(math.min(35, vim.o.lines * 0.5))
+            local row = math.ceil(vim.o.lines - height) * 0.5 - 1
+            local col = math.ceil(vim.o.columns - width) * 0.5 - 1
+
+            local float_config = {
+              width = width,
+              height = height,
+              row = row,
+              col = col,
+              relative = "editor",
+              border = "rounded",
+            }
+
+            local buf = vim.api.nvim_create_buf(false, false)
+            vim.api.nvim_open_win(buf, true, float_config)
+
+            client:open_note(note, { sync = true })
+            client:write_note_to_buffer(note)
           end
         '';
-        options = { desc = "Open Obsidian daily note"; };
+        options = { desc = "Open new note"; };
         mode = "n";
       }
 
