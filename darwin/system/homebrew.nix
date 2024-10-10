bf082a70868c09527391604da0bd809aafbfd4b1
Aaron Pierce <aaron@cute.engineer>
2024-10-10 13:38:56 +1100

    feat(mac): installing a few tools

--- darwin/system/homebrew.nix
+++ darwin/system/homebrew.nix
@@ -32,5 +32,11 @@
       "redis"
       "watchman"
     ];
+
+    masApps = {
+      "flow" = 1423210932; # https://www.flow.app/
+      "just-focus-2" = 1142151959; # https://getjustfocus.com/
+      "dropover" = 1355679052; # https://dropoverapp.com/
+    };
   };
 }
