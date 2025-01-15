bb226e906f193e6a71315dc577b7d0ecbd942725
Aaron Pierce <aaron@cute.engineer>
2025-01-15 15:23:44 +1100

    wip installing alacritty 0.15.0

--- shared/modules/home/alacritty.nix
+++ shared/modules/home/alacritty.nix
@@ -1,10 +1,25 @@
 { pkgs, lib, inputs, ... }:
 let
   inherit (pkgs.hostPlatform) isLinux isDarwin;
+  inherit (pkgs) fetchFromGitHub;
+
+  alacritty-0-15-0 = pkgs.alacritty.overrideAttrs {
+    version = "0.15.0";
+
+    cargoHash = "";
+
+    src = fetchFromGitHub {
+      owner = "alacritty";
+      repo = "alacritty";
+      rev = "5339553";
+      hash = "sha256-CAxf0ltvYXYTdjQmLQnRwRRJUBgABbHSB8DxfAbgBdo=";
+    };
+  };
 in
 {
   programs.alacritty = {
     enable = true;
+    package = alacritty-0-15-0;
 
     # https://alacritty.org/config-alacritty.html
     settings = {
