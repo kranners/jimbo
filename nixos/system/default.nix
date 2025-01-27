19a58d0b15a652956d618cfc6add5d4da34fdf97
Aaron Pierce <aaron@cute.engineer>
2025-01-27 21:44:17 +1100

    feat(nixos): adding ssh server

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -61,6 +61,8 @@
 
   programs.gnupg.agent.enable = true;
 
+  services.openssh.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
