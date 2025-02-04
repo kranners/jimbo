626ae813cd1e0f916e52afd4ae3f3817bde0f9f2
Aaron Pierce <aaron@cute.engineer>
2025-02-04 23:02:14 +1100

    fix(krabby): 🦀

--- shared/modules/home/krabby.nix
+++ shared/modules/home/krabby.nix
@@ -3,11 +3,11 @@
 
   programs.zsh.initExtra = ''
     if [[ -z "$CURRENT_POKEMON" ]]; then
-      export CURRENT_POKEMON="$(krabby random --no-gmax --no-mega --no-regional | awk 'NR == 1 {print tolower($0)}')"
-    fi
+      export CURRENT_POKEMON="$(krabby random --no-gmax --no-mega --no-regional 1-5 | awk 'NR == 1 {print tolower($0)}')"
 
-    if [[ -n "$ALACRITTY_WINDOW_ID" ]]; then
-      alacritty msg config --window-id "$ALACRITTY_WINDOW_ID" "window.title=\"$CURRENT_POKEMON\""
+      if [[ -n "$ALACRITTY_WINDOW_ID" ]]; then
+        alacritty msg config --window-id "$ALACRITTY_WINDOW_ID" "window.title=\"$CURRENT_POKEMON\""
+      fi
     fi
 
     krabby name "$CURRENT_POKEMON" --no-title
