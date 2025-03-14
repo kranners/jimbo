668c8dc3c33c350e0c64d6d425301865d9670244
Aaron Pierce <aaron@cute.engineer>
2025-03-14 18:53:48 +1100

    whoops, not defining wpaperd user service twice

--- nixos/home/wpaperd.nix
+++ nixos/home/wpaperd.nix
@@ -1,27 +1,10 @@
-{ pkgs, config, ... }: {
-  programs.wpaperd = {
+{ config, ... }: {
+  services.wpaperd = {
     enable = true;
-    settings = {
-      default = {
-        path = config.xdg.userDirs.pictures;
-        sorting = "random";
-        duration = "30m";
-      };
+    settings.default = {
+      path = config.xdg.userDirs.pictures;
+      sorting = "random";
+      duration = "30m";
     };
-  };
-  systemd.user.services.wpaperd = {
-    Unit = {
-      Description = "Wayland wallpaper daemon";
-      PartOf = [ config.wayland.systemd.target ];
-      After = [ config.wayland.systemd.target ];
-      ConditionEnvironment = "WAYLAND_DISPLAY";
-    };
-
-    Service = {
-      ExecStart = "${pkgs.wpaperd}/bin/wpaperd";
-      Restart = "on-failure";
-    };
-
-    Install.WantedBy = [ "hyprland-session.target" ];
   };
 }
