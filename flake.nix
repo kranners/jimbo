c1bb3872ccae41ca7de527e64211960c944a3cb3
Aaron Pierce <aaron@cute.engineer>
2025-03-19 12:17:34 +1100

    also removing zen browser, while in the area

--- flake.nix
+++ flake.nix
@@ -21,11 +21,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     alacritty-themes = {
       url = "github:Johnsoct/alacritty";
       flake = false;
