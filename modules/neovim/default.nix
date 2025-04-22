a331e2af9abc27cfee6f9ebcba6db248cbf0cb95
Aaron Pierce <aaron@cute.engineer>
2025-04-22 16:36:04 +1000

    adding new note prompt, better lua flow

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,27 +1,29 @@
 {
-  sharedHomeModule = { pkgs, ... }: {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
+  sharedHomeModule = { pkgs, config, ... }:
+    let
+      inherit (config.lib.file) mkOutOfStoreSymlink;
+      nvimSourceHome = "${config.home.homeDirectory}/workspace/jimbo/modules/neovim";
+    in
+    {
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+      };
 
-    xdg.configFile = {
-      "nvim/init.lua".source = ./init.lua;
-      "nvim/lua".source = ./lua;
-    };
+      xdg.configFile.nvim.source = mkOutOfStoreSymlink nvimSourceHome;
 
-    home.packages = [
-      pkgs.typescript-language-server
-      pkgs.vscode-langservers-extracted
-      pkgs.emmet-language-server
-      pkgs.nixd
-      pkgs.nixpkgs-fmt
-      pkgs.ruff
-      pkgs.python313Packages.jedi-language-server
-      pkgs.fzf
-      pkgs.lua-language-server
-      pkgs.gh
-    ];
-  };
+      home.packages = [
+        pkgs.typescript-language-server
+        pkgs.vscode-langservers-extracted
+        pkgs.emmet-language-server
+        pkgs.nixd
+        pkgs.nixpkgs-fmt
+        pkgs.ruff
+        pkgs.python313Packages.jedi-language-server
+        pkgs.fzf
+        pkgs.lua-language-server
+        pkgs.gh
+      ];
+    };
 }
 
