d4579830f540ac6b54a957357e516028e73d5198
Aaron Pierce <aaron@cute.engineer>
2025-05-05 14:56:55 +1000

    installing yuck grammar, starting main config file

--- modules/eww/default.nix
+++ modules/eww/default.nix
@@ -1,3 +1,11 @@
 {
-  nixosHomeModule.programs.eww.enable = true;
+  nixosHomeModule = { config, ... }:
+    let
+      inherit (config.lib.file) mkOutOfStoreSymlink;
+      ewwSourceHome = "${config.home.homeDirectory}/workspace/jimbo/modules/eww";
+    in
+    {
+      programs.eww.enable = true;
+      xdg.configFile.eww.source = mkOutOfStoreSymlink ewwSourceHome;
+    };
 }
