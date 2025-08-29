a96a52ab04adc6c05ed6e35a8a6e319b3868d19d
Aaron Pierce <aaron@cute.engineer>
2025-08-29 15:47:36 +0700

    fixing lsp configs

--- modules/neovim/lua/plugins/live-command.lua
+++ modules/neovim/lua/plugins/live-command.lua
@@ -1,5 +1,6 @@
 return {
   'smjonas/live-command.nvim',
+  main = 'live-command',
   opts = {
     commands = {
       Norm = { cmd = "norm" },
--- modules/neovim/lua/plugins/nvim-lspconfig.lua
+++ modules/neovim/lua/plugins/nvim-lspconfig.lua
@@ -2,65 +2,70 @@ return {
   'neovim/nvim-lspconfig',
   opts = {
     enabled_servers = {
-      { name = "yamlls" },
-      {
-        extraOptions = {
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "javascript.jsx",
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
+      yamlls = {  },
+      ts_ls = {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "non-relative",
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
           },
-          init_options = {
-            preferences = {
-              importModuleSpecifierPreference = "non-relative",
-              includeInlayEnumMemberValueHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayParameterNameHints = "all",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayVariableTypeHints = true,
+        },
+      },
+      nixd = {
+        settings = {
+          nixd = {
+            formatting = {
+              command = { "nixpkgs-fmt" },
             },
           },
         },
-        name = "ts_ls",
       },
-      { name = "terraformls" },
-      { name = "ruby_lsp" },
-      {
-        extraOptions = {
-          settings = {
-            nixd = {
-              formatting = {
-                command = { "nixpkgs-fmt" },
-              },
-            },
-          },
-        },
-        name = "nixd",
-      },
-      { name = "lua_ls" },
-      { name = "jsonls" },
-      { name = "html" },
-      { name = "gopls" },
-      { name = "eslint" },
-      { name = "emmet_language_server" },
-      { name = "astro" },
-      { name = "ruff" },
-      { name = "jedi_language_server" },
-      {
-        extraOptions = {
-          settings = {
-            basedpyright = {
-              typeCheckingMode = "basic",
-            },
-          },
-        },
-        name = "basedpyright",
-      },
+      lua_ls = {  },
+      jsonls = {  },
+      html = {  },
+      eslint = {  },
+      emmet_language_server = {  },
+      astro = {  },
     },
   },
+  config = function(_, opts)
+    local build_caps = function()
+			local base_caps = vim.lsp.protocol.make_client_capabilities()
+			base_caps.general.positionEncodings = { "utf-8" }
+
+			-- if plugin_enabled("blink.cmp") then
+			-- 	base_caps = require("blink.cmp").get_lsp_capabilities(base_caps)
+			-- end
+
+			return base_caps
+		end
+
+    local default_config = {
+			capabilities = build_caps(),
+			flags = {
+				debounce_text_changes = 150,
+			},
+		}
+
+		for server, config in pairs(opts.enabled_servers) do
+			config = vim.tbl_deep_extend("force", default_config, config)
+
+			vim.lsp.config(server, config)
+			vim.lsp.enable(server)
+		end
+  end
 }
