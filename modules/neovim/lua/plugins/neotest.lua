f8af9ad33759e60b1b6855a764bcb5149c877088
Aaron Pierce <aaron@cute.engineer>
2025-09-04 12:34:06 +0700

    wip adding neotest, works for vitest

---
+++ modules/neovim/lua/plugins/neotest.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-jest",
+    -- "thenbe/neotest-playwright",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-vitest"),
+        require("neotest-jest")({}),
+      },
+    })
+  end,
+  keys = {
+    {
+      "<Leader>tf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run current test file",
+    },
+    {
+      "<Leader>tt",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run nearest test",
+    },
+  },
+}
