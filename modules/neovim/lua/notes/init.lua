ec062f3c77e92ffd94067df71a9d24b71c02c47c
Aaron Pierce <aaron@cute.engineer>
2025-10-21 09:58:19 +1100

    wip moving to obsidian-nvim, moving setup logic

--- modules/neovim/lua/notes/init.lua
+++ modules/neovim/lua/notes/init.lua
@@ -1,35 +1,5 @@
-local constants = require("notes.constants")
 local commands = require("notes.commands")
 local pickers = require("notes.pickers")
-
-local is_darwin = vim.fn.has("macunix") == 1
-
-require("obsidian").setup({
-  daily_notes = { date_format = "%Y/%m/%d %B, %Y", template = "Daily.md" },
-  open_notes_in = "current",
-  templates = { subdir = constants.latte_root .. "/Templates" },
-  disable_frontmatter = true,
-  ui = {
-    checkboxes = {
-      [" "] = { char = " ", hl_group = "ObsidianTodo", order = 100 },
-      ["~"] = { char = "~", hl_group = "ObsidianTilde", order = 200 },
-      ["x"] = { char = "x", hl_group = "ObsidianDone", order = 300 },
-    },
-    enable = false,
-  },
-  workspaces = {
-    { name = "Latte", path = constants.latte_root },
-  },
-  follow_url_func = function(url)
-    if is_darwin then
-      vim.fn.jobstart({ "open", url })
-    end
-
-    if not is_darwin then
-      vim.fn.jobstart({ "xdg-open", url })
-    end
-  end,
-})
 
 vim.keymap.set("n", "<Tab>", commands.prompt_for_new_note)
 vim.keymap.set("n", "<S-Tab>", pickers.stack_notes)
--- modules/neovim/lua/plugins/obsidian.lua
+++ modules/neovim/lua/plugins/obsidian.lua
@@ -5,14 +5,37 @@ return {
   },
   ft = "markdown",
   opts = {
+    legacy_commands = false,
     workspaces = {
       {
         name = "Latte",
         path = "~/Documents/Latte",
       },
     },
+    daily_notes = { date_format = "%Y/%m/%d %B, %Y", template = "Daily.md" },
+    open_notes_in = "current",
+    templates = { subdir = "~/Documents/Latte/Templates" },
+    frontmatter = {
+      enabled = false,
+    },
     ui = {
+      checkboxes = {
+        [" "] = { char = " ", hl_group = "ObsidianTodo", order = 100 },
+        ["~"] = { char = "~", hl_group = "ObsidianTilde", order = 200 },
+        ["x"] = { char = "x", hl_group = "ObsidianDone", order = 300 },
+      },
       enable = false,
     },
+    follow_url_func = function(url)
+      local is_darwin = vim.fn.has("macunix") == 1
+
+      if is_darwin then
+        vim.fn.jobstart({ "open", url })
+      end
+
+      if not is_darwin then
+        vim.fn.jobstart({ "xdg-open", url })
+      end
+    end,
   },
 }
