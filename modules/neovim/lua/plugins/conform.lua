609ec8daa422b75ac03386461855ed8453cc3940
Aaron Pierce <aaron@cute.engineer>
2025-10-21 09:49:40 +1100

    adding eslinting on save

--- modules/neovim/lua/plugins/conform.lua
+++ modules/neovim/lua/plugins/conform.lua
@@ -1,7 +1,14 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    format_on_save = { lsp_fallback = false, timeout_ms = 500 },
+    format_on_save = {
+      -- lsp_fallback = false,
+      timeout_ms = 500,
+      lsp_format = "first",
+      filter = function(client)
+        return client.name == "eslint"
+      end,
+    },
     formatters_by_ft = {
       ruby = { "rubocop" },
       lua = { "stylua" },
