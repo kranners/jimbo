a77748e4bc086840c5286b006680f86afeeb95d8
Aaron Pierce <aaron@cute.engineer>
2025-11-07 11:20:39 +1100

    fixing checkbox order in obsidian

--- modules/neovim/lua/plugins/obsidian.lua
+++ modules/neovim/lua/plugins/obsidian.lua
@@ -6,26 +6,39 @@ return {
   ft = "markdown",
   opts = {
     legacy_commands = false,
+
     workspaces = {
       {
         name = "Latte",
         path = "~/Documents/Latte",
       },
     },
+
     daily_notes = { date_format = "%Y/%m/%d %B, %Y", template = "Daily.md" },
+
     open_notes_in = "current",
+
     templates = { subdir = "~/Documents/Latte/Templates" },
+
     frontmatter = {
       enabled = false,
     },
+
     ui = {
       checkboxes = {
-        [" "] = { char = " ", hl_group = "ObsidianTodo", order = 100 },
-        ["~"] = { char = "~", hl_group = "ObsidianTilde", order = 200 },
-        ["x"] = { char = "x", hl_group = "ObsidianDone", order = 300 },
+        [" "] = { char = " ", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "x", hl_group = "ObsidianDone" },
       },
       enable = false,
     },
+
+    checkbox = {
+      order = {
+        " ",
+        "x",
+      },
+    },
+
     follow_url_func = function(url)
       local is_darwin = vim.fn.has("macunix") == 1
 
