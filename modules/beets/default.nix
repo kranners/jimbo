0c0f88c9eaa38fb72b5da378a72eb66b03b910cd
Aaron <aaron@cute.engineer>
2025-11-24 11:16:32 +1100

    wip adding beets

---
+++ modules/beets/default.nix
@@ -0,0 +1,79 @@
+{
+  sharedHomeModule = { pkgs, ... }: {
+    home.packages = [ pkgs.unflac ];
+
+    programs.beets = {
+      enable = true;
+
+      settings = {
+        plugins = [
+          "badfiles"
+          "scrub"
+          "chroma"
+          "replaygain"
+          "albumtypes"
+          "spotify"
+          "deezer"
+          "lastgenre"
+          "discogs"
+          "fetchart"
+          "embedart"
+          "lyrics"
+          "missing"
+          "info"
+          "inline"
+          "ftintitle"
+          "unimported"
+        ];
+
+        embedart = {
+          remove_art_file = true;
+        };
+
+        replaygain = {
+          overwrite = true;
+          per_disc = true;
+        };
+
+        lastgenre = {
+          count = 5;
+          source = "track";
+        };
+
+        chroma = {
+          auto = true;
+          source_weight = 0.1;
+        };
+
+        discogs = {
+          append_style_genre = true;
+          source_weight = 0.2;
+        };
+
+        spotify.source_weight = 0.3;
+        deezer.source_weight = 0.4;
+
+        item_fields = {
+          multidisc = "1 if disctotal > 1 else 0";
+          first_genre = "genre.split(', ')[0]";
+        };
+
+        # import config, rewrite existing fields
+        import = {
+          set_fields = {
+            # https://github.com/beetbox/beets/pull/4743#issuecomment-2014092144
+            albumartist = "%first{%first{$albumartists,1,0,␀},1,0,\\}";
+          };
+        };
+
+        paths = {
+          default = "$albumartist/$album ($year)/%if{$multidisc,$disc - }\${track}. $title";
+        };
+
+        asciify_paths = true;
+        per_disc_numbering = true;
+        directory = "/media/tower/media/music";
+      };
+    };
+  };
+}
