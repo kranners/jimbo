d2dfe8a497e2335e96b5cde283a9201841e2f61b
Aaron <aaron@cute.engineer>
2026-08-12 14:16:03 +1000

    Add Claude settings, disabling bundled skills

--- modules/claude/default.nix
+++ modules/claude/default.nix
@@ -1,5 +1,33 @@
 {
   sharedHomeModule = {
     home.file.".claude/CLAUDE.md".source = ./CLAUDE.md;
+    home.file.".claude/settings.json".text = builtins.toJSON {
+      theme = "auto";
+
+      # Model settings
+      model = "claude-fable-5[1m]";
+      effortLevel = "high";
+
+      # Plugins
+      enabledPlugins = {
+        "typescript-lsp@claude-plugins-official" = true;
+      };
+
+      # Disable builtin workflows eg code-review
+      skipWorkflowUsageWarning = false;
+      disableBundledSkills = true;
+
+      # Recaps
+      awaySummaryEnabled = false;
+
+      # Disable feedback surveys
+      feedbackSurveyRate = 0;
+
+      # Disable prompt suggestions
+      promptSuggestionsEnabled = false;
+
+      # See full tool usages
+      verbose = true;
+    };
   };
 }
