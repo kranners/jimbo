63b926de3c981532d0919744dea0f927c3abb3b9
Aaron <aaron@cute.engineer>
2026-08-17 10:29:11 +1000

    Add rename-workspace cmux script

--- modules/cmux/default.nix
+++ modules/cmux/default.nix
@@ -1,17 +1,31 @@
 {
-  sharedHomeModule.xdg.configFile.cmux = {
-    target = "./cmux/cmux.json";
+  sharedHomeModule = { pkgs, ... }:
+    let
+      rename-workspace = pkgs.writeShellApplication {
+        name = "rename-workspace";
+        text = ''
+          WORKSPACE_NAME="$(basename "$(pwd)")"
+          cmux workspace-action --action rename --title "$WORKSPACE_NAME"
+        '';
+      };
+    in
+  {
+    home.packages = [rename-workspace];
 
-    text = builtins.toJSON {
-      shortcuts = {
-        bindings = {
-          focusLeft = "ctrl+cmd+h";
-          focusDown = "ctrl+cmd+j";
-          focusUp = "ctrl+cmd+k";
-          focusRight = "ctrl+cmd+l";
+    xdg.configFile.cmux = {
+      target = "./cmux/cmux.json";
 
-          splitRight = "ctrl+cmd+n";
-          splitDown = "ctrl+cmd+shift+n";
+      text = builtins.toJSON {
+        shortcuts = {
+          bindings = {
+            focusLeft = "ctrl+cmd+h";
+            focusDown = "ctrl+cmd+j";
+            focusUp = "ctrl+cmd+k";
+            focusRight = "ctrl+cmd+l";
+
+            splitRight = "ctrl+cmd+n";
+            splitDown = "ctrl+cmd+shift+n";
+          };
         };
       };
     };
