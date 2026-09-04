b899ab901d9c6c629536efb00c9358c54a9eca9d
Aaron <aaron@cute.engineer>
2026-09-04 15:36:39 +1000

    Move nixos into modules

--- nixos/home/plexamp.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.plexamp ];
-
-  wayland.windowManager.sway.config.window.commands = [{
-    criteria = { title = "Plexamp"; };
-    command = "opacity 0.65";
-  }];
-}
